74bb79ddcc7e6bc289e7f09f370616e3334f93e5
Nashali Vicente Lopez <nashalivicente340@gmail.com>
2024-07-08 14:16:13 -0700

    Added select shapes function and move or change color

--- MouseNanny.java
+++ MouseNanny.java
@@ -1,13 +1,25 @@
 package javiergs.gui.paint.gamma;
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
+import java.util.Stack;
 
 public class MouseNanny implements MouseListener, MouseMotionListener {
 	int tmpx, tmpy;
+	private Shape selectedShape = null;
+	int offsetX, offsetY;
+
 
 	public void mousePressed(MouseEvent e) {
 		tmpx = e.getX();
 		tmpy = e.getY();
+
+		selectedShape = getShapeAt(e.getX(), e.getY());
+		if (selectedShape != null){
+			offsetX = tmpx - selectedShape.getX();
+			offsetY = tmpy -selectedShape.getY();
+		}
 	}
 
 	public void mouseReleased(MouseEvent e){
@@ -28,11 +40,17 @@ public class MouseNanny implements MouseListener, MouseMotionListener {
 	}
 
 	public void mouseDragged(MouseEvent e) {
-		int x = e.getX();
-		int y = e.getY();
-		int[] p = calculateShapeParameters(x, y);
-		Officer.setShapeParameters(p);
-		Officer.tellYourBoss();
+		if (selectedShape != null){
+			selectedShape.setX(e.getX() - offsetX);
+			selectedShape.setY(e.getY() - offsetY);
+			Officer.tellYourBoss();
+		} else {
+			int x = e.getX();
+			int y = e.getY();
+			int[] p = calculateShapeParameters(x, y);
+			Officer.setShapeParameters(p);
+			Officer.tellYourBoss();
+		}
 	}
 
 	private int[] calculateShapeParameters(int x, int y) {
@@ -55,8 +73,66 @@ public class MouseNanny implements MouseListener, MouseMotionListener {
 		return parameters;
 	}
 
-	public void mouseClicked(MouseEvent e) {
+	private Shape getShapeAt(int x, int y){
+		Stack<Shape> shapes = Officer.getShapeStack();
+		for(int i = shapes.size() - 1; i >= 0; i--){
+			Shape shape = shapes.get(i);
+			if(shapeContains(shape, x, y)){
+				return shape;
+			}
+		}
+		return null;
+	}
 
+	private boolean shapeContains(Shape shape, int x, int y){
+		if (shape instanceof Rectangle || shape instanceof Circle || shape instanceof Arc){
+			return x >= shape.getX() && x <= shape.getX() + shape.getWidth() &&
+					y >= shape.getY() && y <= shape.getY() + shape.getHeight();
+		}else if (shape instanceof  Line){
+			int tolerance = 5;
+			return distanceFromLine(shape.getX(), shape.getY(), shape.getX() + shape.getWidth(), shape.getY() + shape.getHeight(), x, y) <= tolerance;
+		}
+		return false;
+	}
+
+	private double distanceFromLine(int x1, int y1, int x2, int y2, int x, int y){
+		double A = x - x1;
+		double B = y - y1;
+		double C = x2 - x1;
+		double D = y2 - y1;
+
+		double dot = A * C + B * D;
+		double len_sq = C * C + D * D;
+		double param = -1;
+		if(len_sq != 0)
+			param = dot/len_sq;
+
+		double xx, yy;
+
+		if(param < 0){
+			xx = x1;
+			yy = y1;
+		}else if(param > 1){
+			xx = x2;
+			yy = y2;
+		}else{
+			xx = x1 + param * C;
+			yy = y1 + param * D;
+		}
+
+		double dx = x - xx;
+		double dy  = y -yy;
+		return Math.sqrt(dx * dx + dy * dy);
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		if(e.getClickCount() == 2 && selectedShape != null){
+			Color newColor = JColorChooser.showDialog(null, "Choose a color", selectedShape.getColor());
+			if(newColor != null){
+				selectedShape.setColor(newColor);
+				Officer.tellYourBoss();
+			}
+		}
 	}
 	public void mouseEntered(MouseEvent e) {}
 	public void mouseExited(MouseEvent e) {}
